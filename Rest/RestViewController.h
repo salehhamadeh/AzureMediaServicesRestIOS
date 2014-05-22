d7568662eb0f64148110442e62e7caa06ca50492
Saleh Hamadeh <salehhamadeh@Salehs-MacBook-Pro.local>
2014-05-22 14:39:02 -0400

    Initial Commit

---
+++ Rest/RestViewController.h
@@ -0,0 +1,13 @@
+//
+//  RestViewController.h
+//  Rest
+//
+//  Created by Saleh Hamadeh on 5/22/14.
+//  Copyright (c) 2014 Spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestViewController : UIViewController
+
+@end
---
+++ Rest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rest
+//
+//  Created by Saleh Hamadeh on 5/22/14.
+//  Copyright (c) 2014 Spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RestAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RestAppDelegate class]));
+    }
+}
